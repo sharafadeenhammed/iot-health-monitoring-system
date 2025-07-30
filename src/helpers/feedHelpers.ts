b32a551a76df0986a7e6ce2a8adf80c68d864fd6
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-07-30 18:49:11 +0100

    implemented feed data normalization

---
+++ src/helpers/feedHelpers.ts
@@ -0,0 +1,26 @@
+import type { Feed, NormarlizedFeedData } from "../../types/feed"
+
+
+const normalizeFeedData = (feed: Feed): NormarlizedFeedData => {
+  const data: NormarlizedFeedData = {
+    ...feed.channel,
+    field1Name: "Ambient Temperature",
+    field2Name: "Humidity",
+    field3Name: "Body Temperature",
+    field4Name: "Heart Beat Rate Per Minite",
+    field5Name: "Blood Oxygen Level",
+    ambientTemperature: feed.feeds[ 0 ].field1,
+    humidity: feed.feeds[ 0 ].field2,
+    bodyTemperature: feed.feeds[ 0 ].field3,
+    heartBeatRate: feed.feeds[ 0 ].field4,
+    bloodOxygenLevel: feed.feeds[ 0 ].field5
+  };
+  return data;
+}
+
+
+const FeedHelpers = {
+  normalizeFeedData,
+}
+
+export default FeedHelpers
---
+++ types/feed.d.ts
@@ -0,0 +1,52 @@
+export interface Feed {
+  channel: {
+    id: number;
+    name: string;
+    latitude: string;
+    longitude: string;
+    field1: string;
+    field2: string;
+    field3: string;
+    field4: string;
+    field5: string;
+    created_at: string;
+    updated_at: string;
+    last_entry_id: number;
+  };
+  feeds: [
+    {
+      created_at: string;
+      entry_id: number;
+      field1: string;
+      field2: string;
+      field3: string;
+      field4: string;
+      field5: string;
+    }
+  ];
+}
+
+export interface NormarlizedFeedData {
+  id: number;
+  name: string;
+  latitude: string;
+  longitude: string;
+  field1: string;
+  field2: string;
+  field3: string;
+  field4: string;
+  field5: string;
+  field1Name: "Ambient Temperature";
+  field2Name: "Humidity";
+  field3Name: "Body Temperature";
+  field4Name: "Heart Beat Rate Per Minite";
+  field5Name: "Blood Oxygen Level";
+  created_at: string;
+  updated_at: string;
+  last_entry_id: number;
+  ambientTemperature: string;
+  humidity: string;
+  bodyTemperature: string;
+  heartBeatRate: string;
+  bloodOxygenLevel: string;
+}
