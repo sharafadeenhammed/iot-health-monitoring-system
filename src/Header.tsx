f13031782dc893f3d105d5306971df3b422c5ae0
sharafadeen hammed <sharafadeenhammed@yahoo.com>
2025-07-31 08:54:06 +0100

    created header component

---
+++ src/Header.tsx
@@ -0,0 +1,26 @@
+import { IoMdHeart } from "react-icons/io";
+import moment from "moment";
+
+interface HeaderProps {
+  updatedAt?: string;
+}
+function Header({ updatedAt }: HeaderProps) {
+  return (
+    <div className='sticky top-0 left-0 right-0 py-14 px-5 bg-blue-950 text-slate-300 rounded-b-[20px]'>
+      {updatedAt ? (
+        <p className='font-mono absolute top-2 right-2 text-right sm:text-center'>
+          Last reading updated: {moment(updatedAt).fromNow()}
+        </p>
+      ) : null}
+      <div className='flex justify-between items-start '>
+        <p className='text-4xl font-mono font-extrabold text-left'>
+          IOT Based <br /> Health Monitoring System{" "}
+        </p>
+
+        <IoMdHeart className='text-slate-300 animate-pulse' size={100} />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
